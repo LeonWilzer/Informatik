ec5eea37d090a20eae396246426ab843121d73e1
Leon Wilzer <leon.wilzer@protonmail.com>
2021-04-28 14:12:37 +0200

    user admin comp.

--- src/UserAdministration/Benutzerverwaltung.java
+++ src/UserAdministration/Benutzerverwaltung.java
@@ -4,20 +4,27 @@ import lib.BinarySearchTree;
 
 public class Benutzerverwaltung {
     private BinarySearchTree<Benutzerprofil> benutzerBaum;
-    public Benutzerverwaltung(){}
+    public Benutzerverwaltung(){
+        benutzerBaum = new BinarySearchTree<Benutzerprofil>();
+    }
 
     public void neuenNutzerAnlegen(String pBenutzernamen, String pPW)
     {
-
+        benutzerBaum.insert(new Benutzerprofil(pBenutzernamen, pPW));
     }
 
     public void nutzerLoeschen(String pBenutzernamen, String pPW)
     {
-        
+        benutzerBaum.remove(new Benutzerprofil(pBenutzernamen, pPW));
     }
 
-    public boolean profilVorhanden(String pBenutzername)
+    public boolean profilVorhanden(String pBenutzernamen)
     {
+        return benutzerBaum.search(new Benutzerprofil(pBenutzernamen, null)) != null;
+    }
 
+    public Benutzerprofil getBenutzerprofil(String pBenutzernamen)
+    {
+        return benutzerBaum.search(new Benutzerprofil(pBenutzernamen, null));
     }
 }
