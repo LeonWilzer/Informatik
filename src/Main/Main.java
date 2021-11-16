28ab0ba1d353761027efe2cb4b8cc2ffb67dd3aa
Leon Wilzer <leon.wilzer@protonmail.com>
2021-11-16 16:05:32 +0100

    Chat added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,8 @@
 package Main;
 
 import lib.TIO;
+import Chat.ChatClient;
+import Chat.ChatServer;
 import Morse.Morse;
 import Nikolaus.Nikolaus;
 import SozialesNetzwerk.SozialesNetzwerk;
@@ -25,37 +27,39 @@ public class Main {
             "4. Benutzerverwaltung\n" +
             "5. Haus des Nikolaus\n" +
             "6. Zeichenbaum\n" +
-            "7. Soziales Netzwerk"
+            "7. Soziales Netzwerk\n" +
+            "8. Chat Client\n" +
+            "9. Chat Server"
             );
+        TIO.cls();
         switch(choice)
         {
             case 1:
-                TIO.cls();
                 TIO.prt(new Morse().decode(TIO.AskString("Please provide some morse code:")));
                 break;
             case 2:
-                TIO.cls();
                 TIO.prt(new Morse().encodeString(TIO.AskString("Please provide some clear text:")));
                 break;
             case 3:
-                TIO.cls();
                 Termtree.Demo();
                 break;
             case 4:
-                TIO.cls();
                 Benutzerverwaltung.Demo();
                 break;
             case 5:
-                TIO.cls();
                 ZeichenVerwaltung.Demo();
                 break;
             case 6:
-                TIO.cls();
                 new Nikolaus();
                 break;
             case 7:
-                TIO.cls();
                 new SozialesNetzwerk();
+                break;
+            case 8:
+                new ChatClient(TIO.AskString("Server IP?"), TIO.AskInt("Server Port?"), TIO.AskString("Username?")).run();
+                break;
+            case 9:
+                new ChatServer(TIO.AskInt("Port?"), TIO.AskString("Server Name?"));
                 break;
         }
     }
