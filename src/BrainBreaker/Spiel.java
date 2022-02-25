ad00c560029783b0bbc3b89f4544e7175a470399
Leon Wilzer <leon.wilzer@protonmail.com>
2022-02-25 13:58:13 +0100

    idk

--- src/BrainBreaker/Spiel.java
+++ src/BrainBreaker/Spiel.java
@@ -13,25 +13,32 @@ public class Spiel {
     {
         Random rnd = new Random();
         anzahlAbgegebenerTipps= 0;
-        tipps = new List<Ziffernfolge>;
-        String code = rnd.nextInt(origin
-                , bound)
-        geheimerCode = new Ziffernfolge(p)
+        tipps = new List<Ziffernfolge>();
+        String code = "";
+        code +=  rnd.nextInt(6) + rnd.nextInt(6) + rnd.nextInt(6)+rnd.nextInt(6);
+        geheimerCode = new Ziffernfolge(code);
     }
     public void fuegeTippsHinzu(Ziffernfolge pTipp)
     {
         if(pTipp == null)
             return;
 
-        
+        tipps.append(pTipp);
+        anzahlAbgegebenerTipps++;
     }
     public int gibAnzahlTipps(){
-
+        return anzahlAbgegebenerTipps;
     }
 
     public Ziffernfolge gibTipp(int pIndex)
     {
-
-
+        tipps.toFirst();
+        int i = 0;
+        while(tipps.hasAccess() && i<pIndex)
+        {
+            tipps.next();
+            i++;
+        }
+        return tipps.getContent();
     }
 }
