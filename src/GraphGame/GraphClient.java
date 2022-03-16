3d8dda7245125877a11540240daa88250b298bbf
Lukas Langrock <lukas.langrock@mailbox.org>
2022-03-16 09:21:39 +0100

    logical client code structure draft

--- src/GraphGame/GraphClient.java
+++ src/GraphGame/GraphClient.java
@@ -3,14 +3,50 @@ package GraphGame;
 import lib.Client;
 
 public class GraphClient extends Client {
+    GraphClientTUI tui;
+    char proposedTurnIndex;
+    char proposedTurnColour;
 
     public GraphClient(String pServerIP, int pServerPort) {
         super(pServerIP, pServerPort);
-        // TODO Auto-generated constructor stub
+        tui = new GraphClientTUI();
     }
+
+    private 
 
     @Override
     public void processMessage(String pMessage) {
+        String words[] = pMessage.split("|");
+
+        switch(words[0]) {
+            // the used protocol is just a concept draft
+            case "OK":
+                turnCornfirm();
+                break;
+
+            case "YOUR_TURN":
+                turnStart();
+                break;
+            
+            case "SET_COLOUR":
+                // example protocol: SET_COLOUR|B|RED
+                break;
+        }
+    }
+
+    private void turnPropose() {
+        // TODO
+    }
+
+    private void turnCornfirm() {
+        // TODO
+    }
+
+    private void turnStart() {
+        // TODO
+    }
+
+    private void updateGraph(char index, char colour) {
         // TODO
     }
 }
---
+++ src/GraphGame/GraphClientTUI.java
@@ -0,0 +1,31 @@
+package GraphGame;
+
+import lib.TIO;
+
+public class GraphClientTUI {
+    TIO tio;
+
+    public GraphClientTUI() {
+        tio = new TIO();
+    }
+
+    // start intro
+    public void setStart() {
+        // TODO
+    }
+
+    // ask user for their turn
+    public void getTurn() {
+        // TODO
+    }
+
+    // tell user about a confirmed turn either by the player or the opponent
+    public void setTurn() {
+        // TODO
+    }
+
+    // tell user about who won the game
+    public void setFinish() {
+        // TODO
+    }
+}
